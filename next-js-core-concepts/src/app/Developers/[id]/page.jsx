796f11fc5f7e01c9b6fc70de1c22e94011585073
Affnan Sawad <affnansawad2002@gmail.com>
2025-04-13 15:41:22 +0600

    1

--- next-js-core-concepts/src/app/Developers/[id]/page.jsx
+++ next-js-core-concepts/src/app/Developers/[id]/page.jsx
@@ -1,53 +1,59 @@
 import React from 'react';
 
-const DevData = [
-  {
-    id: 1,
-    title: "Junior Software Engineer",
-    company: "TechNova Inc.",
-    location: "New York, NY",
-    skills: ["JavaScript", "React", "HTML", "CSS"],
-    experienceLevel: "Entry-Level",
-    salary: 91193,
-    currency: "USD",
-    remote: true
-  },
-  {
-    id: 2,
-    title: "Junior Backend Developer",
-    company: "CloudCore Solutions",
-    location: "New York, NY",
-    skills: ["Node.js", "Express", "MongoDB"],
-    experienceLevel: "Entry-Level",
-    salary: 91193,
-    currency: "USD",
-    remote: false
-  },
-  {
-    id: 3,
-    title: "Junior Full Stack Developer",
-    company: "InnoTech Labs",
-    location: "New York, NY",
-    skills: ["React", "Node.js", "TypeScript"],
-    experienceLevel: "Entry-Level",
-    salary: 91193,
-    currency: "USD",
-    remote: true
-  },
-  {
-    id: 4,
-    title: "Junior Web Developer",
-    company: "PixelPoint",
-    location: "New York, NY",
-    skills: ["HTML", "CSS", "JavaScript", "Tailwind CSS"],
-    experienceLevel: "Entry-Level",
-    salary: 91193,
-    currency: "USD",
-    remote: false
-  }
-];
+
 
 const IdPage = ({ params }) => {
+ 
+    console.log(params.id)
+
+    const DevData = [
+        {
+          id: 1,
+          title: "Junior Software Engineer",
+          company: "TechNova Inc.",
+          location: "New York, NY",
+          skills: ["JavaScript", "React", "HTML", "CSS"],
+          experienceLevel: "Entry-Level",
+          salary: 91193,
+          currency: "USD",
+          remote: true
+        },
+        {
+          id: 2,
+          title: "Junior Backend Developer",
+          company: "CloudCore Solutions",
+          location: "New York, NY",
+          skills: ["Node.js", "Express", "MongoDB"],
+          experienceLevel: "Entry-Level",
+          salary: 91193,
+          currency: "USD",
+          remote: false
+        },
+        {
+          id: 3,
+          title: "Junior Full Stack Developer",
+          company: "InnoTech Labs",
+          location: "New York, NY",
+          skills: ["React", "Node.js", "TypeScript"],
+          experienceLevel: "Entry-Level",
+          salary: 91193,
+          currency: "USD",
+          remote: true
+        },
+        {
+          id: 4,
+          title: "Junior Web Developer",
+          company: "PixelPoint",
+          location: "New York, NY",
+          skills: ["HTML", "CSS", "JavaScript", "Tailwind CSS"],
+          experienceLevel: "Entry-Level",
+          salary: 91193,
+          currency: "USD",
+          remote: false
+        }
+      ];
+
+
   const dev = DevData.find(dev => dev.id == params.id);
 
   if (!dev) {
---
+++ next-js-core-concepts/src/app/error.jsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react';
+
+const error = () => {
+    return (
+        <div>
+           no page found LOL ! 
+        </div>
+    );
+};
+
+export default error;
